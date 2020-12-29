17b2c65395c61e3f19fc5fa1218cef956eb52c5f
vuhang1712 <vuhang1939@gmail.com>
2020-12-29 17:47:16 +0700

    create GLsurfaceView

--- app/src/main/java/com/example/arcore_seo4_nhom121/MainActivity.java
+++ app/src/main/java/com/example/arcore_seo4_nhom121/MainActivity.java
@@ -1,18 +1,55 @@
 package com.example.arcore_seo4_nhom121;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        glSurfaceView = findViewById(R.id.surfaceview);
+
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new GLSurfaceRenderer());
+        setContentView(glSurfaceView);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    private class GLSurfaceRenderer implements GLSurfaceView.Renderer {
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+
+        }
+    }
 }
